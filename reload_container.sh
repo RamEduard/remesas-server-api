1523cce2ddb1fb6952d14370e85de7248d56b1b2
Ramon Serrano <rserrano1994@pop-os.localdomain>
2021-04-15 11:39:43 -0300

    Actualizado script reload_container.

--- reload_container.sh
+++ reload_container.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-export APP_NAME=remesas-server-api
+export APP_NAME=react-remesas-app
+export APP_NAME_V2=react-arbitr-app
+export APP_SERVER_NAME=remesas-server-api
 
 echo "Reload container..."
 
 docker pull registry.gitlab.com/rameduard/remesas-server-api:master
+
+docker pull registry.gitlab.com/rameduard/react-remesas-app:master
+
+docker pull registry.gitlab.com/rameduard/react-arbitr-app:master
 
 echo "Stop $APP_NAME container..."
 
@@ -12,8 +18,32 @@ docker container stop $(docker container ls -q --filter name=$APP_NAME)
 
 echo "Remove $APP_NAME container..."
 
-docker container rm $(docker container ls -q --filter name=$APP_NAME)
+docker container rm $(docker container ls -qa --filter name=$APP_NAME)
+
+echo "Stop $APP_NAME_V2 container..."
+
+docker container stop $(docker container ls -q --filter name=$APP_NAME_V2)
+
+echo "Remove $APP_NAME_V2 container..."
+
+docker container rm $(docker container ls -qa --filter name=$APP_NAME_V2)
+
+echo "Stop $APP_SERVER_NAME container..."
+
+docker container stop $(docker container ls -q --filter name=$APP_SERVER_NAME)
+
+echo "Remove $APP_SERVER_NAME container..."
+
+docker container rm $(docker container ls -qa --filter name=$APP_SERVER_NAME)
+
+echo "Run a new $APP_SERVER_NAME container..."
+
+docker run -d --name="$APP_SERVER_NAME" -p 8080:8080 --network remesas registry.gitlab.com/rameduard/remesas-server-api:master
 
 echo "Run a new $APP_NAME container..."
 
-docker run -d --name remesas-server-api -p 8080:8080 --network remesas registry.gitlab.com/rameduard/remesas-server-api:master
+docker run -d --name="$APP_NAME" -p 3000:80 --network remesas registry.gitlab.com/rameduard/react-remesas-app:master
+
+echo "Run a new $APP_NAME_V2 container..."
+
+docker run -d --name="$APP_NAME_V2" -p 3001:80 --network remesas registry.gitlab.com/rameduard/react-arbitr-app:master
